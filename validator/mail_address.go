d6d01bfe80f838b75674086c4314d79decb671a8
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-10-22 22:01:02 -0400

    fix: Made mail address validator globally accessible

--- validator/mail_address.go
+++ validator/mail_address.go
@@ -2,7 +2,7 @@ package validator
 
 import "net/mail"
 
-func validMailAddress(address string) (string, bool) {
+func ValidMailAddress(address string) (string, bool) {
 	addr, err := mail.ParseAddress(address)
 	if err != nil {
 		return "", false

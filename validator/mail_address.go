498c165e83346d26d98db139653bd2662b770c57
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-10-22 21:57:23 -0400

    feat: Added mail address validator

---
+++ validator/mail_address.go
@@ -0,0 +1,11 @@
+package validator
+
+import "net/mail"
+
+func validMailAddress(address string) (string, bool) {
+	addr, err := mail.ParseAddress(address)
+	if err != nil {
+		return "", false
+	}
+	return addr.Address, true
+}

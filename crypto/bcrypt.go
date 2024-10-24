fce3c714721e453ca7a16e7d892366e054428556
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-10-24 14:46:06 -0400

    feat: Added bcrypt hash functions
    * Added bcrypt hash password, check password hash and is hashed functions

---
+++ crypto/bcrypt.go
@@ -0,0 +1,39 @@
+package crypto
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes a password using bcrypt
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+// CheckPasswordHash checks if the password matches the hash
+func CheckPasswordHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// IsHashed checks if the password is hashed
+// IsHashed checks if a string is a bcrypt hash
+func IsHashed(str string) bool {
+	// bcrypt hashes are always 60 characters long
+	if len(str) != 60 {
+		return false
+	}
+
+	// Try to decode the hash
+	err := bcrypt.CompareHashAndPassword([]byte(str), []byte{})
+	return errors.Is(err, bcrypt.ErrMismatchedHashAndPassword)
+}

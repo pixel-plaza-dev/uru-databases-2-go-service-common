f590d55ccf4cfb4acdf80940cf56833ad80ea188
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-10-26 17:52:01 -0400

    fix: Solved bug on ValidNonEmptyStringFields
    I guess...

--- validator/non_empty_string_fields.go
+++ validator/non_empty_string_fields.go
@@ -10,12 +10,23 @@ func ValidNonEmptyStringFields(validations *map[string][]error, data interface{}
 	// Reflection of data
 	dataReflection := reflect.ValueOf(data)
 
+	// If data is a pointer, dereference it
+	if dataReflection.Kind() == reflect.Ptr {
+		dataReflection = dataReflection.Elem()
+	}
+
 	// Iterate over the fields
 	for _, fieldName := range *fields {
 		// Get field
-		field := dataReflection.FieldByName(fieldName).String()
+		field := dataReflection.FieldByName(fieldName)
 
-		if len(field) == 0 {
+		// Dereference the field if it is a pointer
+		if field.Kind() == reflect.Ptr {
+			field = field.Elem()
+		}
+
+		// Check if the field is a string and is empty
+		if field.Kind() == reflect.String && len(field.String()) == 0 {
 			(*validations)[fieldName] = append((*validations)[fieldName], validator.StringIsEmptyError{Field: fieldName})
 		}
 	}

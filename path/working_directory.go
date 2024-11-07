556b73fa3f5ab4068bfc0343798d2f13386d7d58
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-06 22:05:33 -0400

    feat: Added GetCurrentDirectory function

---
+++ path/working_directory.go
@@ -0,0 +1,16 @@
+package path
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// GetCurrentDirectory gets the current directory of the executable
+func GetCurrentDirectory() (string, error) {
+	execPath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Dir(execPath), nil
+}

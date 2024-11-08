395f6c0e6020d3799accb944d236323f12fcbba3
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-08 14:18:27 -0400

    refactor: Modified LoadUri function and renamed as LoadServiceUri

--- grpc/loader.go
+++ grpc/loader.go
@@ -3,39 +3,14 @@ package grpc
 import (
 	enverror "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/env/error"
 	"os"
-	"strings"
 )
 
-// Uri is the URI for the gRPC service
-type Uri struct {
-	Host string
-	Port string
-	Uri  string
-}
-
-// LoadUri loads the URI from the environment
-func LoadUri(hostKey string, portKey string) (*Uri, error) {
-	// Get host from environment
-	host, exists := os.LookupEnv(hostKey)
-	if !exists {
-		return nil, enverror.VariableNotFoundError{Key: hostKey}
+// LoadServiceURI load service URI from environment variables
+func LoadServiceURI(key string) (uri string, err error) {
+	// Get environment variable
+	uri, uriExists := os.LookupEnv(key)
+	if !uriExists {
+		return "", enverror.VariableNotFoundError{Key: key}
 	}
-
-	// Get port from environment
-	port, exists := os.LookupEnv(portKey)
-	if !exists {
-		return nil, enverror.VariableNotFoundError{Key: portKey}
-	}
-
-	// Build URI string
-	var uriBuilder strings.Builder
-	uriBuilder.WriteString(host)
-	uriBuilder.WriteString(":")
-	uriBuilder.WriteString(port)
-
-	return &Uri{
-		Host: host,
-		Port: port,
-		Uri:  uriBuilder.String(),
-	}, nil
+	return uri, nil
 }

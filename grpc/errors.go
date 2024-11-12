ca76f3cde11a6c5d9a6168061bffda809ac1d0cf
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-11 21:54:42 -0400

    feat: Added LoadTLSCredentials function

--- grpc/errors.go
+++ grpc/errors.go
@@ -3,8 +3,9 @@ package grpc
 import "errors"
 
 var (
-	InternalServerError = errors.New("internal server error")
-	ServiceUnavailable  = errors.New("service unavailable")
-	Unauthenticated     = errors.New("unauthenticated")
-	Unauthorized        = errors.New("unauthorized")
+	InternalServerError   = errors.New("internal server error")
+	ServiceUnavailable    = errors.New("service unavailable")
+	Unauthenticated       = errors.New("unauthenticated")
+	Unauthorized          = errors.New("unauthorized")
+	FailedToAddCAPemError = errors.New("failed to add server CA's certificate")
 )
--- grpc/loader.go
+++ grpc/loader.go
@@ -1,7 +1,10 @@
 package grpc
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	enverror "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/env/error"
+	"google.golang.org/grpc/credentials"
 	"os"
 )
 
@@ -13,4 +16,28 @@ func LoadServiceURI(key string) (uri string, err error) {
 		return "", enverror.VariableNotFoundError{Key: key}
 	}
 	return uri, nil
+}
+
+// LoadTLSCredentials loads the TLS credentials
+func LoadTLSCredentials(pemServerCAPath string) (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := os.ReadFile(pemServerCAPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a certificate pool from the certificate
+	certPool := x509.NewCertPool()
+
+	// Append the certificates from the PEM file
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, FailedToAddCAPemError
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
 }

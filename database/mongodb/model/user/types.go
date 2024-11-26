dda80d232b722025f0cbda7444e86ad8bfb6df98
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-26 18:41:40 -0400

    refactor: Removed 'is_active' field from UserEmail and UserPhoneNumber types

--- database/mongodb/model/user/types.go
+++ database/mongodb/model/user/types.go
@@ -24,7 +24,6 @@ type UserEmail struct {
 	AssignedAt time.Time          `json:"assigned_at" bson:"assigned_at"`
 	VerifiedAt time.Time          `json:"verified_at,omitempty" bson:"verified_at,omitempty"`
 	RevokedAt  time.Time          `json:"revoked_at,omitempty" bson:"revoked_at,omitempty"`
-	IsActive   bool               `json:"is_active" bson:"is_active"`
 	IsPrimary  bool               `json:"is_primary" bson:"is_primary"`
 }
 
@@ -36,7 +35,6 @@ type UserPhoneNumber struct {
 	AssignedAt  time.Time          `json:"assigned_at" bson:"assigned_at"`
 	VerifiedAt  time.Time          `json:"verified_at,omitempty" bson:"verified_at,omitempty"`
 	RevokedAt   time.Time          `json:"revoked_at,omitempty" bson:"revoked_at,omitempty"`
-	IsActive    bool               `json:"is_active" bson:"is_active"`
 }
 
 // UserEmailVerification is the MongoDB for the user email verification entity

06dcd3f9813e9916b7400327a457a7df5b224e1c
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-26 19:05:39 -0400

    feat: Added PrepareFindOneOptions function

--- database/mongodb/utils.go
+++ database/mongodb/utils.go
@@ -3,6 +3,7 @@ package mongodb
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // GetObjectIdFromString gets the object ID from the string
@@ -18,4 +19,22 @@ func GetObjectIdFromString(id string) (*primitive.ObjectID, error) {
 		return nil, err
 	}
 	return &objectId, nil
+}
+
+// PrepareFindOneOptions prepares the find one options
+func PrepareFindOneOptions(projection interface{}, sort interface{}) *options.FindOneOptions {
+	// Create the find options
+	findOptions := options.FindOne()
+
+	// Set the projection
+	if projection != nil {
+		findOptions.SetProjection(projection)
+	}
+
+	// Set the sort
+	if sort != nil {
+		findOptions.SetSort(sort)
+	}
+
+	return findOptions
 }

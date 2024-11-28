8fc9fd175a99d1b1948b50cb6120105f7b5c6423
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-28 00:01:51 -0400

    feat: Added PrepareFindOptions and PrepareUpdateOptions functions

--- database/mongodb/utils.go
+++ database/mongodb/utils.go
@@ -38,3 +38,42 @@ func PrepareFindOneOptions(projection interface{}, sort interface{}) *options.Fi
 
 	return findOptions
 }
+
+// PrepareFindOptions prepares the find options
+func PrepareFindOptions(projection interface{}, sort interface{}, limit int64, skip int64) *options.FindOptions {
+	// Create the find options
+	findOptions := options.Find()
+
+	// Set the projection
+	if projection != nil {
+		findOptions.SetProjection(projection)
+	}
+
+	// Set the sort
+	if sort != nil {
+		findOptions.SetSort(sort)
+	}
+
+	// Set the limit
+	if limit > 0 {
+		findOptions.SetLimit(limit)
+	}
+
+	// Set the skip
+	if skip > 0 {
+		findOptions.SetSkip(skip)
+	}
+
+	return findOptions
+}
+
+// PrepareUpdateOptions prepares the update options
+func PrepareUpdateOptions(upsert bool) *options.UpdateOptions {
+	// Create the update options
+	updateOptions := options.Update()
+
+	// Set the upsert
+	updateOptions.SetUpsert(upsert)
+
+	return updateOptions
+}
